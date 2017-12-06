243f9d204091c48660f71012fb1d5a13a3d4dd38
Érico Rolim <erico.nogueira@unicamperacing.com>
2017-12-05 23:23:15 -0200

    Add property decorators, and update setter interfaces
    Tornar impossível a alteração das propriedades importantes do programa, e ao mesmo tempo tornando mais fácil a substituição de valores conhecidos. Ainda há problemas na implementação de incertezas_conhecidas.

--- fis_exp/classe_principal.py
+++ fis_exp/classe_principal.py
@@ -1,32 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sympy as sp
 
 class FisExp:
 
-	def __init__ (self, funcao, valores_conhecidos={}):
+	def __init__ (self, funcao, valores_conhecidos=dict(), incertezas_conhecidas=dict()):
 		
 		if type(funcao) is str:
-			self.funcao = sp.sympify(funcao)
+			self.__funcao = sp.sympify(funcao)
 		else:
-			self.funcao = funcao
+			self.__funcao = funcao
 
-		self.variaveis = self.funcao.atoms(sp.Symbol)
-		self.incertezas = {}
-		for simbolo in self.variaveis:
+		self.__variaveis = self.__funcao.atoms(sp.Symbol)
+		self.__incertezas = {}
+		for simbolo in self.__variaveis:
 			incerteza = sp.symbols('u_' + str(simbolo))
-			self.incertezas[simbolo] = incerteza
+			self.__incertezas[simbolo] = incerteza
 
-		self.funcao_substituida = self.funcao
-		self.valores_conhecidos = dict()
-		self.substituicao(valores_conhecidos)
+		self.__funcao_substituida = self.__funcao
+
+		self.__valores_conhecidos = dict()
+		self.valores_conhecidos = valores_conhecidos
+
+		self.__incertezas_conhecidas = dict()
 		self._propagacao()
+		self.incertezas_conhecidas = incertezas_conhecidas
 
-	def substituicao (self, valores_conhecidos):
-		'''Gera o atributo funcao_substituida, que é a função com os valores conhecidos para algumas variáveis
+	@property
+	def funcao (self):
+		return self.__funcao
+	@funcao.setter
+	def funcao (self, a):
+		pass
+
+	@property
+	def variaveis (self):
+		return self.__variaveis
+	@variaveis.setter
+	def variaveis(self, a):
+		pass
+
+	@property
+	def incertezas (self):
+		return self.__incertezas
+	@incertezas.setter
+	def incertezas (self, a):
+		pass
+
+	@property
+	def valores_conhecidos (self):
+		return self.valores_conhecidos
+	@valores_conhecidos.setter
+	def valores_conhecidos (self, valores_conhecidos):
+		'''Gera o atributo "funcao_substituida", que é a função com os valores conhecidos para algumas variáveis
+		É chamado por self.valores_conhecidos = dict com valores
+		Para apagar os valores substituídos, é necessário usar self.valores_conhecidos = {}
 		'''
 		for var in valores_conhecidos.keys():
-			self.valores_conhecidos[var] = valores_conhecidos[var]
-			self.funcao_substituida = self.funcao.subs(self.valores_conhecidos)
-		return self.funcao_substituida
+			self.__valores_conhecidos[var] = valores_conhecidos[var]
+
+		if not valores_conhecidos:
+			self.__valores_conhecidos = dict()
+			self.__funcao_substituida = self.__funcao
+		else:
+			self.__funcao_substituida = self.__funcao.subs(self.__valores_conhecidos)
+		
+	@property
+	def funcao_substituida (self):
+		return self.__funcao_substituida
+	@funcao_substituida.setter
+	def funcao_substituida (self, a):
+		pass
+
+	@property
+	def incertezas_conhecidas (self):
+		return self.__incertezas_conhecidas
+	@incertezas_conhecidas.setter
+	def incertezas_conhecidas (self, incertezas_conhecidas):
+		'''Gera o atributo "propagacao_substituida", que é a função de propagação com os valores 
+		conhecidos para algumas variáveis.
+		É chamado por self.incertezas_conhecidas = dict com valores
+		'''
+		for inc in incertezas_conhecidas.keys():
+			self.__incertezas_conhecidas[inc] = incertezas_conhecidas[inc]
+
+		incertezas_chaves_corretas = dict()
+		for chave_variavel in self.__variaveis:
+			if chave_variavel in self.__incertezas_conhecidas.keys():
+				incertezas_chaves_corretas[self.__incertezas[chave_variavel]] = self.__incertezas_conhecidas[chave_variavel]
+
+		propagacao_substituida = self.__propagacao.subs(incertezas_chaves_corretas)
+		propagacao_substituida = propagacao_substituida.subs(self.__valores_conhecidos)
+
+		self.propagacao_substituida = propagacao_substituida
+
+	@property
+	def propagacao (self):
+		return self.__propagacao
+	@propagacao.setter
+	def propagacao (self, a):
+		pass
 
 	def _propagacao (self):
 		'''Calcula a propagação de incertezas da instância do objeto. A propagação utilizada é do tipo
@@ -34,10 +108,10 @@ class FisExp:
 		e utiliza como nome para cada incerteza "u_VARIAVEL".
 		'''
 		propagacao = sp.sympify('0')
-		for var in self.variaveis:
-			propagacao += self.derivar(str(var))**2*self.incertezas[var]**2
+		for var in self.__variaveis:
+			propagacao += self.derivar(str(var))**2*self.__incertezas[var]**2
 
-		self.propagacao = sp.sqrt(propagacao)
+		self.__propagacao = sp.sqrt(propagacao)
 
 	def integrar (self, var, limites=None):
 		'''Permite integrar facilmente a função na variável "var" (que deve vir como string), e
@@ -45,13 +119,13 @@ class FisExp:
 		'''
 		self._gerar_simbolo(var)
 		if limites is None:
-			return self.funcao.integrate(self._ultimo_simbolo)
+			return self.__funcao.integrate(self._ultimo_simbolo)
 		else:
-			return self.funcao.integrate((simboloelf._ultimo_simbolo, limites[0], limites[1]))
+			return self.__funcao.integrate((simboloelf._ultimo_simbolo, limites[0], limites[1]))
 
 	def derivar (self, var, ponto_avaliado=None, indice=1):
 		'''Permite derivar facilmente a função na variável "var" (que deve vir como string), e
-		se o parâmetro "ponto_avaliado" for configurado, avalia a função derivada lá.
+		se o parâmetro "ponto_avaliado" for configurado, avalia a função derivada naquele ponto.
 		Se o parâmetro "indice" for utilizado, será calculada a derivada daquele grau
 		'''
 		if var is not str:
@@ -59,9 +133,9 @@ class FisExp:
 		else:
 			self._gerar_simbolo(var)
 		if ponto_avaliado is None:
-			return self.funcao.diff(self._ultimo_simbolo, indice)
+			return self.__funcao.diff(self._ultimo_simbolo, indice)
 		else:
-			return self.funcao.diff(self._ultimo_simbolo, indice).subs(self._ultimo_simbolo, ponto_avaliado)
+			return self.__funcao.diff(self._ultimo_simbolo, indice).subs(self._ultimo_simbolo, ponto_avaliado)
 
 	def _gerar_simbolo (self, var):
 		'''Para permitir a interação com os símbolos internos do programa, é gerado um símbolo
@@ -72,7 +146,7 @@ class FisExp:
 		return self._ultimo_simbolo
 
 	def __str__ (self):
-		return str(self.funcao)
+		return str(self.__funcao)
 
 '''def gerarSimbolos (lista, dict):
 	exec ('import sympy as sp')
@@ -81,9 +155,18 @@ class FisExp:
 #ideia a ser implementada
 
 if __name__ is "__main__":
-	f = sp.sympify('a+b+c')
+	f = sp.sympify('x*a+b+c')
 	f_a = FisExp(f)
-	print ("Funcao: ", f_a.funcao)
+	print ("Funcao: ", str(f_a))
 	print ("Propagacao: ", f_a.propagacao)
 	variaveis = {'a':4}
-	print ("Substituicao: ", f_a.substituicao(variaveis))
+	incertezas = {'a':1}
+	f_a.valores_conhecidos = variaveis
+	f_a.incertezas_conhecidas = incertezas
+	print ("Substituicao: ", f_a.funcao_substituida)
+	f_a.funcao_substituida = 'bla'
+	print("Teste de substituicao: ", f_a.funcao_substituida)
+	variaveis = dict()
+	f_a.valores_conhecidos = variaveis
+	print("Teste de substituicao 2: ", f_a.funcao_substituida)
+	print ('Propagacao substituida: ', f_a.propagacao_substituida)
