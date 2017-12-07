0e9b8b35b2be8390cec8fa668a1838649d74724d
Érico Rolim <erico.nogueira@unicamperacing.com>
2017-12-07 01:22:24 -0200

    Update README, add PyPI url

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='aluno_exatas',
-    version='0.1dev',
+    version='0.3',
     packages=['aluno_exatas'],
     license='MIT License',
     long_description=open('README.md').read(),
-   	#url='http://pypi.python.org/pypi/FisExp',
+   	url='http://pypi.python.org/pypi/AlunoExatas',
     author='EricoNR',
     author_email='e170610@g.unicamp.br'
 )
